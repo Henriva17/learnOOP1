99ac5d4ed3b5e7ac0d5cade8d0cc9fc29f186c43
Henriva17 <henritresor405@gmail.com>
2026-03-18 15:35:02 +0100

    javaSkills

---
+++ src/main/java/com/henri/Application.java
@@ -0,0 +1,17 @@
+package com.henri;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello java");
+        int student1Score = 12;
+        int javaSkills = 10;
+        int finalScore = student1Score  + javaSkills;
+        if (finalScore >= 20){
+            System.out.println("student worked well");
+        } else {
+            System.out.println("Keep pusshing");
+        }
+
+
+    }
+}
